c3ad919e422e44b444e760201e583f3b81187a64
Nikita Makarevich <nikita_makarevich@yahoo.com>
2023-12-08 00:52:05 +0100

    Add 'Restart' and correct ChoiceBox rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Collapse } from 'react-bootstrap';
 import ChoiceBox from './components/ChoiceBox';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,11 +9,31 @@ import story from './assets/dishonored_story.json';
 import StoryPointText from './components/StoryPointText';
 
 function App() {
+    // const [currStorypointName, setCurrStorypointName] = useState("start");
+    const [currStorypoint, setCurrStorypoint] = useState(story.storypoints["start"]);
+    const [key, setKey] = useState(0);
+    const [openChoiceBox, setOpenChoiceBox] = useState(false);
+    
+    const restart = (event) => {
+        setCurrStorypoint(story.storypoints["start"]);
+        setOpenChoiceBox(false);
+        setKey(prevKey => prevKey + 1);
+    };
+
+    const showChoices = () => {
+        setOpenChoiceBox(true);
+    }
+
     return (
         <>
             <h1>{story.title}</h1>
-            <StoryPointText storypoint={story.storypoints.start} />
-            <ChoiceBox prompt={story.storypoints.start.prompt} choices={story.storypoints.start.choices} />
+            <Button onClick={restart} type="primary">Restart</Button>
+            <StoryPointText key={key} storypoint={currStorypoint} onFinished={showChoices}/>
+            <Collapse in={openChoiceBox}>
+                <div>
+                    {openChoiceBox && <ChoiceBox prompt={currStorypoint.prompt} choices={currStorypoint.choices} />}
+                </div>
+            </Collapse>
         </>
     )
 }
--- src/components/StoryPointText.jsx
+++ src/components/StoryPointText.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import "./common.css";
 import "./StoryPointText.css";
 
-export default function StoryPointText({ storypoint }) {
+export default function StoryPointText({ storypoint, onFinished}) {
     const [textIndex, setTextIndex] = useState(0);
     const [isFinalText, setIsFinalText] = useState(false);
 
@@ -10,16 +10,14 @@ export default function StoryPointText({ storypoint }) {
         if (textIndex < storypoint.texts.length-1) {
             if(textIndex+1 == storypoint.texts.length-1) {
                 setIsFinalText(true);
+                onFinished();
             }
             setTextIndex(textIndex + 1);
         }
-        else {
-
-        };
     };
 
     return (
-        <div className="story-text-box" onClick={handleClick}>
+        <div className={"story-text-box" + ((isFinalText) ? "" : " clickable")} onClick={handleClick}>
             <p>{storypoint.texts[textIndex]}</p>
             {(!isFinalText) ? (<p className="weak-prompt">[Click to continue]</p>) : null}      
         </div>
